ba3a58cb1b7a93ad2ef53b7bbfac6b898ad45c98
Marcos Celiz <mceliz@gmail.com>
2025-03-11 17:02:29 -0300

    ajustes endpoint url

--- app/api/terms_and_conditions_api.py
+++ app/api/terms_and_conditions_api.py
@@ -38,8 +38,9 @@ class TermsAndConditionsListResource(Resource):
             return terms_last_version.serialize(), 200
         return {'message': 'No terms and conditions found'}, 404
 
+class TermsAndConditionsLatestResource(Resource):
     def get(self, language_code):
-        terms_last_version = TermsAndConditionsService.get_latest_version(language_code)
+        terms_last_version = TermsAndConditionsService.get_latest_version_language(language_code)
         if terms_last_version:
             return terms_last_version.serialize(), 200
         return {'message': f'No terms and conditions found for language: {language_code}'}, 404 
@@ -64,4 +65,5 @@ class AcceptTermsResource(Resource):
 
 api.add_resource(TermsAndConditionsResource, '/terms/<int:terms_id>')
 api.add_resource(TermsAndConditionsListResource, '/terms')
+api.add_resource(TermsAndConditionsLatestResource, '/terms/<string:language_code>')
 api.add_resource(AcceptTermsResource, '/users/<int:user_id>/accept-terms')
