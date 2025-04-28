7a7dbce16b93baf88d8275adafda0a436bc0b349
Juan Javier Pedernera <102430306+Javier-Pedernera@users.noreply.github.com>
2025-04-28 18:55:26 -0300

    agrego contact content

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -17,6 +17,7 @@ from app.models.status import Status
 from app.models.promotion_consumed import PromotionConsumed
 from app.models.terms_and_conditions import TermsAndConditions
 from app.models.city import City
+from app.models.contact_content import ContactContent
 # Importa aquí otros modelos a medida que los crees
 
 # Si necesitas inicializar algo específicamente para los modelos, puedes hacerlo aquí.
